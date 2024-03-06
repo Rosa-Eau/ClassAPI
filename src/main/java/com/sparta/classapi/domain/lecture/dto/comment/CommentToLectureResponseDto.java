f53736e15fabc7d5e3556a36ad1d31652a77b92f
weiß Yi <78130010+rosa-eau@users.noreply.github.com>
2024-03-07 01:33:06 +0900

    :boom: Small Changes, Big Difference

--- src/main/java/com/sparta/classapi/domain/lecture/dto/comment/CommentToLectureResponseDto.java
+++ src/main/java/com/sparta/classapi/domain/lecture/dto/comment/CommentToLectureResponseDto.java
@@ -3,23 +3,15 @@ package com.sparta.classapi.domain.lecture.dto.comment;
 import com.sparta.classapi.domain.lecture.entity.comment.Comment;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-
 @Getter
 public class CommentToLectureResponseDto {
     private String email;
 
     private String content;
 
-    private LocalDateTime registeredAt;
-
-    private LocalDateTime updatedAt;
-
     public CommentToLectureResponseDto(Comment comment) {
         this.email = comment.getUser().getEmail();
         this.content = comment.getContent();
-        this.registeredAt = comment.getRegisteredAt();
-        this.updatedAt = comment.getUpdatedAt();
     }
 
 }
